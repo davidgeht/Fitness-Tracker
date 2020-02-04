23a7fe16cafb035e33c3e2cddf2fc7f78e77d081
David Gehtman <davidgehtman.91@gmail.com>
2020-02-04 17:23:35 -0500

    added server.js file

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const htmlroutes = require('./routes/html-routes');
+const apiroutes = require('./routes/api-routes');
+
+const path = require("path");
+
+let db = require("./models");
+ 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{useNewUrlParser: true} );
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(apiroutes);
+app.use(htmlroutes);
+
+
+app.listen(PORT, ()=>{
+    console.log(`App running on port ${PORT}!`);
+});
